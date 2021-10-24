2e208ee53f4386823a9e6121236f0281fd10c07f
asdia0 <asdia.the.potato@gmail.com>
2021-10-24 19:55:25 +0800

    Make Board properties private

--- Minesweeper/Board.cs
+++ Minesweeper/Board.cs
@@ -1,53 +1,14 @@
 ﻿namespace Minesweeper
 {
-    using System;
     using System.Collections.Generic;
 
     public class Board
     {
-        private bool lengthSet = false;
+        private int Length;
 
-        private bool breadthSet = false;
+        private int Breadth;
 
-        private int _Length;
-
-        private int _Breadth;
-
-        public int Length
-        {
-            get
-            {
-                return this._Length;
-            }
-
-            set
-            {
-                if (!this.lengthSet)
-                {
-                    this._Length = value;
-                    this.lengthSet = true;
-                }
-            }
-        }
-
-        public int Breadth
-        {
-            get
-            {
-                return this._Breadth;
-            }
-
-            set
-            {
-                if (!this.breadthSet)
-                {
-                    this._Breadth = value;
-                    this.breadthSet = true;
-                }
-            }
-        }
-
-        public List<Cell> Cells { get; set; }
+        private List<Cell> Cells { get; set; }
 
         public Board(int length, int breadth)
         {
