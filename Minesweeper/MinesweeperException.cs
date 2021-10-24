daa7a7bd0c9abee91e80ede7766c1319644966bb
asdia0 <asdia.the.potato@gmail.com>
2021-10-24 20:20:50 +0800

    Add MinesweeperException

---
+++ Minesweeper/MinesweeperException.cs
@@ -0,0 +1,24 @@
+﻿namespace Minesweeper
+{
+    using System;
+
+    /// <summary>
+    /// Defines an exception thrown in this project.
+    /// </summary>
+    public class MinesweeperException : Exception
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MinesweeperException"/> class.
+        /// </summary>
+        public MinesweeperException()
+        { }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MinesweeperException"/> class.
+        /// </summary>
+        /// <param name="message">A message about the exception.</param>
+        public MinesweeperException(string message)
+            : base(message)
+        { }
+    }
+}
