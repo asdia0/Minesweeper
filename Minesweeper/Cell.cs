8b3dc06f93cfc11ddafde22e3155101bd91bb060
asdia0 <asdia.the.potato@gmail.com>
2021-10-24 19:58:39 +0800

    Add Cell

---
+++ Minesweeper/Cell.cs
@@ -0,0 +1,19 @@
+﻿namespace Minesweeper
+{
+    public class Cell
+    {
+        public bool IsFlagged = false;
+
+        public bool HasMine;
+
+        public Cell(bool hasMine)
+        {
+            this.HasMine = hasMine;
+        }
+
+        public void SwitchFlagState()
+        {
+            this.IsFlagged ^= true;
+        }
+    }
+}
