bc2562a7d7af714a8a4991c58b028d2239984ff1
Eytan Chong <asdia.the.potato@gmail.com>
2023-03-21 23:57:34 +0800

    Add Program.cs

---
+++ src/Minesweeper.Analysis/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Minesweeper.Analysis
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Collections.Immutable;
+    using System.Linq;
+    using System.Security.Principal;
+    using Newtonsoft.Json;
+
+    public class Program
+    {
+        public static void Main()
+        {
+
+        }
+    }
+}
