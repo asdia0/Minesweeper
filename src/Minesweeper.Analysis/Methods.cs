284eb74cc435c6647f9c381138527867b35d290f
Eytan Chong <asdia.the.potato@gmail.com>
2023-03-21 22:56:33 +0800

    Add IslandCount

--- src/Minesweeper.Analysis/Methods.cs
+++ src/Minesweeper.Analysis/Methods.cs
@@ -1,7 +1,42 @@
 ﻿namespace Minesweeper.Analysis
 {
+    using System.Collections.Generic;
+    using System.Linq;
+
     public class Methods
     {
+        public static int IslandCount(Grid grid)
+        {
+            int count = 0;
 
+            List<Cell> cells = grid.Cells.Where(i => i.Count != 0).ToList();
+
+            List<Cell> searched = new();
+
+            List<Cell> toSearch = new();
+
+            while (searched.Count() != cells.Count())
+            {
+                Cell seed = null;
+
+                if (toSearch.Any())
+                {
+                    seed = toSearch.First();
+                }
+                else
+                {
+                    count++;
+                    // select another seed
+                    seed = cells.Except(searched).First();
+                }
+
+                // add cells if 1) orthogonally adjacent to seed, 2) in cells, 3) not in searched
+                toSearch.AddRange(seed.OrthogonallyAdjacentCells.Intersect(cells).Except(searched).Except(toSearch).ToList());
+                toSearch.Remove(seed);
+                searched.Add(seed);
+            }
+
+            return count;
+        }
     }
 }
