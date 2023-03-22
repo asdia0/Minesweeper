94ba2efc6c80d487fc1a176fa2485d95f7f2671c
Eytan Chong <asdia.the.potato@gmail.com>
2023-03-23 05:54:30 +0800

    Add DensityToMines

--- src/Minesweeper.Analysis/Utility.cs
+++ src/Minesweeper.Analysis/Utility.cs
@@ -34,5 +34,24 @@
 
             return centroid;
         }
+
+        /// <summary>
+        /// Converts mine density to mines.
+        /// Mind density must be between 0 and 1 inclusive.
+        /// </summary>
+        /// <param name="length">The length of the <see cref="Grid">grid</see>.</param>
+        /// <param name="width">The width of the <see cref="Grid">grid</see>.</param>
+        /// <param name="density">The density of mines on the <see cref="Grid">grid</see>. Calculated as mines/(<paramref name="length"/>*<paramref name="width"/>).</param>
+        /// <returns></returns>
+        /// <exception cref="System.Exception"></exception>
+        public static int DensityToMines(int length, int width, double density)
+        {
+            if (density > 1 || density < 0)
+            {
+                throw new System.Exception("Invalid density.");
+            }
+
+            return (int)(length * width * density);
+        }
     }
 }
