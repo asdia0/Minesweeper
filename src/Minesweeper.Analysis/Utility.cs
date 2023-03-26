7d7a7902b4531fa0ffd1e86087ce8bcf1f016114
Eytan Chong <asdia.the.potato@gmail.com>
2023-03-27 01:41:10 +0800

    Generalise NormaliseDistribution

--- src/Minesweeper.Analysis/Utility.cs
+++ src/Minesweeper.Analysis/Utility.cs
@@ -6,19 +6,16 @@
     internal class Utility
     {
         /// <summary>
-        /// Normalises a dataset.
+        /// Normalises a list.
         /// </summary>
-        /// <param name="distribution">The dataset to normalise.</param>
+        /// <param name="distribution">The list to normalise.</param>
         /// <returns></returns>
-        public static List<(double, double)> NormaliseCount(List<(double, double)> distribution)
+        public static List<double> NormaliseCount(List<double> distribution)
         {
-            double max1 = distribution.Max(i => i.Item1);
-            double min1 = distribution.Min(i => i.Item1);
+            double max = distribution.Max();
+            double min = distribution.Min();
 
-            double max2 = distribution.Max(i => i.Item2);
-            double min2 = distribution.Min(i => i.Item2);
-
-            return distribution.Select(i => ((double)(i.Item1 - min1)/(max1 - min1), (double)(i.Item2 - min2)/(max2 - min2))).ToList();
+            return distribution.Select(i => (double)(i - min)/(max - min)).ToList();
         } 
     }
 }
