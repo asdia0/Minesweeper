dbf2e6cc0e5105b81976af7855a444aa23612ba5
Eytan Chong <asdia.the.potato@gmail.com>
2023-03-26 15:21:00 +0800

    Move DensityToMines to Minesweeper.Utility

--- src/Minesweeper/Utility.cs
+++ src/Minesweeper/Utility.cs
@@ -49,5 +49,25 @@
         {
             return coordinates.x + (coordinates.y * width);
         }
+
+        /// <summary>
+        /// Converts mine density to mines.
+        /// Mind density must be between 0 and 1 inclusive.
+        /// </summary>
+        /// <param name="length">The length of the <see cref="Grid">grid</see>.</param>
+        /// <param name="width">The width of the <see cref="Grid">grid</see>.</param>
+        /// <param name="density">The density of mines on the <see cref="Grid">grid</see>.
+        ///     Calculated as mines/(<paramref name="length"/>*<paramref name="width"/>).</param>
+        /// <returns>The number of mines closest to the given <paramref name="density">density</paramref>.</returns>
+        /// <exception cref="System.Exception">Throws an exception if the density is not strictly between 0 and 1.</exception>
+        public static int DensityToMines(int length, int width, double density)
+        {
+            if (density > 1 || density < 0)
+            {
+                throw new System.Exception("Invalid density.");
+            }
+
+            return (int)(length * width * density);
+        }
     }
 }
