1e782099053c873ee094b7b2036a02a2c42c66d3
Eytan Chong <eytanchong88@gmail.com>
2023-11-25 03:13:52 +0800

    Remove Utility.NormaliseCount()

--- src/Minesweeper.Analysis/Utility.cs
+++ src/Minesweeper.Analysis/Utility.cs
@@ -5,17 +5,6 @@
 
     internal class Utility
     {
-        /// <summary>
-        /// Normalises a list.
-        /// </summary>
-        /// <param name="distribution">The list to normalise.</param>
-        /// <returns></returns>
-        public static List<double> NormaliseCount(List<double> distribution)
-        {
-            double max = distribution.Max();
-            double min = distribution.Min();
 
-            return distribution.Select(i => (double)(i - min)/(max - min)).ToList();
-        } 
     }
 }
